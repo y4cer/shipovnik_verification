1a50a41f9b783be6299d8d8ebb33b68abc7e378e
y4cer <me@y4cer.ru>
2024-10-14 06:06:17 +0000

    add dotproduct

--- ref-jasmin/src/main.c
+++ ref-jasmin/src/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+int N = 5;
+int K = 4;
+
+int* func(int* v1);
+
 int main() {
-    printf("Hello, world\n!");
+    int v1[] = {1, 2, 3, 4, 5};
+    int v2[] = {2, 2, 2, 2, 2};
+    int v3[] = {2, 2, 2, 2, 2};
+    int* res;
+
+    res = func(v1);
+    printf("%p\n", res);
+    printf("%d\n", *res);
 }
