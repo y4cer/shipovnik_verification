2a9d6a7bc28a49952e535efff712a428c23f712e
y4cer <me@y4cer.ru>
2024-10-16 04:58:03 +0000

    add dot product

--- ref-jasmin/src/main.c
+++ ref-jasmin/src/main.c
@@ -1,17 +1,18 @@
+#include <stdint.h>
 #include <stdio.h>
 
 int N = 5;
 int K = 4;
 
-int* func(int* v1);
+int dot_product(uint64_t* l, uint64_t* r, const uint64_t len);
 
 int main() {
-    int v1[] = {1, 2, 3, 4, 5};
-    int v2[] = {2, 2, 2, 2, 2};
-    int v3[] = {2, 2, 2, 2, 2};
-    int* res;
+    uint64_t v1[] = {1, 2, 3, 4, 5};
+    uint64_t v2[] = {2, 2, 2, 2, 2};
+    uint64_t v3[] = {2, 2, 2, 2, 2};
 
-    res = func(v1);
-    printf("%p\n", res);
-    printf("%d\n", *res);
+    int res = dot_product(v1, v2, N) & 0xff;
+
+    printf("%x\n", res);
+    printf("%d\n", res);
 }
