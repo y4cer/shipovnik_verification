ea6725ded0fcdc61ecd3d894a7df0af06ef4401a
y4cer <me@y4cer.ru>
2024-11-06 04:58:54 +0000

    fix dot product

--- ref-jasmin/src/main.c
+++ ref-jasmin/src/main.c
@@ -4,14 +4,14 @@
 int N = 5;
 int K = 4;
 
-int dot_product(uint64_t* l, uint64_t* r, const uint64_t len);
+int dot_product(uint64_t* l, uint64_t* r);
 
 int main() {
     uint64_t v1[] = {1, 2, 3, 4, 5};
     uint64_t v2[] = {2, 2, 2, 2, 2};
     uint64_t v3[] = {2, 2, 2, 2, 2};
 
-    int res = dot_product(v1, v2, N) & 0xff;
+    int res = dot_product(v1, v2) & 0xff;
 
     printf("%x\n", res);
     printf("%d\n", res);
